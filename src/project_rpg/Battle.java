e71ab1da23ed30b9217a4ca830e07875ea488f5e
Sinho Chewi <chewisinho@berkeley.edu>
2016-01-08 18:55:50 -0800

    Added Battle.

---
+++ src/project_rpg/Battle.java
@@ -0,0 +1,48 @@
+package project_rpg;
+
+/** Simulates a battle between the player and a monster.
+ *  @author S. Chewi, T. Nguyen, A. Tran
+ */
+public class Battle {
+
+    /** Constructs a new battle between PLAYER and MONSTER. */
+    public Battle(Player player, Monster monster) {
+        _player = player;
+        _monster = monster;
+        System.out.println(_monster.getName() + " has appeared!");
+        System.out.printf("You have %s/%s HP remaining.\n", _player.getHP(),
+            _player.getMaxHP());
+    }
+
+    /** Checks whether the battle is won. */
+    boolean isWon() {
+        return _monster.isDead();
+    }
+
+    /** Checks whether the battle is lost. */
+    boolean isLost() {
+        return _player.isDead();
+    }
+
+    /** Plays a turn where the player uses skill at INDEX. */
+    void useSkill(int index) {
+        int playerDamage = _player.getSkill(index).attack();
+        _monster.reduceHealth(playerDamage);
+        System.out.printf("You did %s damage! The monster has %s HP "
+            + "remaining.\n", playerDamage, Math.max(_monster.hp, 0));
+        if (!_monster.isDead()) {
+            int monsterDamage = _monster.attack();
+            _player.reduceHealth(monsterDamage);
+            System.out.printf("The enemy did %s damage! You have %s/%s HP "
+                + "remaining.\n", monsterDamage, _player.getHP(),
+                _player.getMaxHP());
+        }
+    }
+
+    /** The player in the battle. */
+    private Player _player;
+
+    /** The monster in the battle. */
+    private Monster _monster;
+
+}
