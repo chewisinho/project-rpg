d5313fe989f3bbc9228b211ba501577249c267f6
Alexander Tran <exand.tran@berkeley.edu>
2016-01-08 16:11:25 -0800

    updated Assignment

--- src/project_rpg/Assignment.java
+++ src/project_rpg/Assignment.java
@@ -1,6 +1,7 @@
 package project_rpg;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 
 /** Abstract class representing assignments of a course.
  *  @author S. Chewi, T. Nguyen, A. Tran
@@ -11,13 +12,28 @@ public class Assignment implements Serializable {
     public Assignment(String title) {
         _title = title;
     }
+    
+    /** Constructor for assignment that takes in an array of MONSTERS and
+     * a Title. */
+    public Assignment(ArrayList<Monster> monsters, String title) {
+    	_monsters = monsters;
+    	_title = title;
+    }
 
     /** Returns title of the assignment. */
     public String getTitle() {
         return _title;
     }
+    
+    /** Returns the monsters of the assignment. */
+    public ArrayList<Monster> getMonsters() {
+    	return _monsters;
+    }
 
     /** Title of the assignment. */
     private String _title;
+    
+    /** The monsters in this assignment. */
+    private ArrayList<Monster> _monsters;
 
 }
