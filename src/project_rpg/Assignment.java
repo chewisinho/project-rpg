4fe4e6e9e29ef8e0d34a38f6d6a3d4228f68d4d6
Sinho Chewi <chewisinho@berkeley.edu>
2016-06-29 21:51:13 -0700

    Clean up style for assignment

--- src/project_rpg/Assignment.java
+++ src/project_rpg/Assignment.java
@@ -4,44 +4,47 @@ import java.io.IOException;
 import java.io.Serializable;
 import java.util.ArrayList;
 
-/** Abstract class representing assignments of a course.
- *  @author S. Chewi, T. Nguyen, A. Tran
+/** Abstract class representing assignments of a course. Essentially, assignments just hold
+ *  a dungeon and useful metadata (such as the name and description).
+ *  @author S. Chewi, A. Tran
  */
 public class Assignment implements Serializable {
 
-    /** Constructor for an assignment that takes in a COURSE, a DESCRIPTION, a DUNGEON, and a NAME. */
-    public Assignment(Course course, String description, String dungeon, String name) {
-        _course = course;
-        _description = description;
-        _dungeon = dungeon;
-        _name = name;
-    }
+  /** Constructor for an assignment that takes in a COURSE, a DESCRIPTION, a DUNGEON, and a NAME. */
+  public Assignment(Course course, String description, String dungeon, String name) {
+    this.course = course;
+    this.description = description;
+    this.dungeon = dungeon;
+    this.name = name;
+  }
 
-    /** Returns the description of the assignment. */
-    public String description() {
-        return _name + ": " + _description;
-    }
+  /** Returns the description of the assignment. */
+  public String description() {
+    return name + ": " + description;
+  }
 
-    /** Returns the course in which the assignment is found. */
-    Course getCourse() {
-        return _course;
-    }
-    
-    /** Returns the name of the dungeon for the assignment. */
-    String getDungeon() {
-        return _dungeon;
-    }
+  /** Returns the course in which the assignment is found. */
+  public Course getCourse() {
+    return course;
+  }
+  
+  /** Returns the name of the dungeon for the assignment. */
+  public String getDungeon() {
+    return dungeon;
+  }
 
-    /** Returns the name of the assignment. */
-    public String getName() {
-        return _name;
-    }
+  /** Returns the name of the assignment. */
+  public String getName() {
+    return name;
+  }
 
-    /** The course in which the assignment is found. */
-    private Course _course;
+  /** The course in which the assignment is found. */
+  private Course course;
 
-    /** Title and description of the assignment. */
-    private String _description, _dungeon, _name;
+  /** Title and description of the assignment. */
+  private String description;
+  private String dungeon;
+  private String name;
 
 }
 
